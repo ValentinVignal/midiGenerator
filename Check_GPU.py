14c0875111f7547bec1d8c68b68d5322fbbbf375
Valentin <valentin.vignal@gmail.com>
2019-06-24 11:06:22 +0800

    Add option to check GPU :flashlight:

--- Check_GPU.py
+++ Check_GPU.py
@@ -1,15 +1,29 @@
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 import sys
+import argparse
 
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    # return [x.name for x in local_device_protos if x.device_type == 'GPU']
+    return local_device_protos
 
 def main():
-    def get_available_gpus():
-        local_device_protos = device_lib.list_local_devices()
-        # return [x.name for x in local_device_protos if x.device_type == 'GPU']
-        return local_device_protos
-    name = get_available_gpus()
-    print("GPUS:{}".format(name))
+
+    parser = argparse.ArgumentParser(description='Program to train a model over a midi dataset')
+    parser.add_argument('-l', '--list-local-devices', default=False, action='store_true',
+                        help='Use tf.python.client.device_lib.list_local_devices()')
+    parser.add_argument('-g', '--gpu-device-name', default=False, action='store_true',
+                        help='Use tf.test.gpu_device_name()')
+
+    args = parser.parse_args()
+
+    if args.list_local_devices:
+        name = get_available_gpus()
+        print("Devices:{0}".format(name))
+    if args.gpu_device_name:
+        print('GPU : {0}'.format(tf.test.gpu_device_name()))
+
 
     # Try using gpu
 
