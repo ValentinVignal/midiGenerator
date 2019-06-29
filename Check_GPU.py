bf3d9b6a2c93faa6b7c44eb9bf576571db5e26bc
Valentin <valentin.vignal@gmail.com>
2019-06-29 17:17:16 +0800

    Change presentation :bar_chart:

--- Check_GPU.py
+++ Check_GPU.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 from tensorflow.python.client import device_lib
-import sys
 import argparse
+
 
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
     # return [x.name for x in local_device_protos if x.device_type == 'GPU']
     return local_device_protos
+
 
 def main():
 
@@ -23,7 +24,6 @@ def main():
         print("Devices:{0}".format(name))
     if args.gpu_device_name:
         print('GPU : {0}'.format(tf.test.gpu_device_name()))
-
 
     # Try using gpu
 
