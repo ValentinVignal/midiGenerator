8378c519a493dea0d2385690652d91392c8f2fc5
Valentin <valentin.vignal@gmail.com>
2019-09-01 17:35:10 +0800

    fix bug

--- src/NN/callbacks.py
+++ src/NN/callbacks.py
@@ -20,9 +20,9 @@ class LossHistory(tf.keras.callbacks.Callback):
         while Path('tests_hp/Summary_test_{0}.txt'.format(i)).exists():
             i += 1
         self.path = Path('tests_hp/Summary_test_{0}.txt'.format(i))
+        Path('tests_hp').mkdir(parents=True, exist_ok=True)
         with open(self.path.as_posix(), 'w') as f:
             f.write('\n')
-        Path('tests_hp').mkdir(parents=True, exist_ok=True)
 
     def on_train_begin(self, logs={}):
         self.current_logs = None
