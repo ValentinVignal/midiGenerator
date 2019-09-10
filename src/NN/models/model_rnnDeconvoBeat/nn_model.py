44c8f1f46871587968d33dc8b948ef904b7f5b5a
Valentin <valentin.vignal@gmail.com>
2019-09-10 20:42:13 +0800

    Put back Timedistributed for BatchNormalization

--- src/NN/models/model_rnnDeconvoBeat/nn_model.py
+++ src/NN/models/model_rnnDeconvoBeat/nn_model.py
@@ -93,7 +93,7 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
             x = layers.Conv3D(filters=size, kernel_size=(1, 5, 5), padding='same')(x)
             x = layers.LeakyReLU()(x)
             if batch_norm:
-                x = mlayers.BatchNormalization()(x)
+                x = layers.TimeDistributed(mlayers.BatchNormalization())(x)
             x = layers.Dropout(dropout / 2)(x)
         x = layers.MaxPool3D(pool_size=(1, 3, 3), strides=(1, 1, 2), padding='same')(x)
     shape_before_fc = x.shape
@@ -105,7 +105,7 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
         x = layers.TimeDistributed(layers.Dense(size))(x)
         x = layers.LeakyReLU()(x)
         if batch_norm:
-            x = mlayers.BatchNormalization()(x)
+            x = layers.TimeDistributed(mlayers.BatchNormalization())(x)
         x = layers.Dropout(dropout)(x)
     # ---------- LSTM -----------
     size_before_lstm = x.shape[2]  # (batch, nb_steps, size)
@@ -120,7 +120,7 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
                         recurrent_dropout=dropout)(x)  # (batch, nb_steps, size)
         x = layers.LeakyReLU()(x)
         if batch_norm:
-            x = mlayers.BatchNormalization()(x)
+            x = layers.TimeDistributed(mlayers.BatchNormalization())(x)
         x = layers.Dropout(dropout)(x)
     # -- Last one --
     if lstm_state:
@@ -153,7 +153,7 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
         if batch_norm:
             x = mlayers.BatchNormalization()(x)
         if all_sequence:
-            x = mlayers.BatchNormalization()(x)
+            x = layers.TimeDistributed(mlayers.BatchNormalization())(x)
             x = layers.Flatten()(x)
     x = layers.Dropout(dropout)(x)
     x = layers.Dense(size_before_lstm)(x)  # (batch, size)
