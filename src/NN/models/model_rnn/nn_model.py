415bbe75b66f039ae6ec9959967cd1a4a09106f0
Valentin <valentin.vignal@gmail.com>
2019-09-20 23:20:41 +0800

    Change name of model rnnDeconvoBeat :right_arrow: rnn

--- src/NN/models/model_rnn/nn_model.py
+++ src/NN/models/model_rnn/nn_model.py
@@ -6,12 +6,22 @@ import src.global_variables as g
 
 layers = tf.keras.layers
 Lambda = tf.keras.layers.Lambda
+K = tf.keras.backend
 
 """
 
-First personal model
+LSTM with encoder convolutional layers
 
 """
+
+
+def min_max_pool3d(x):
+    min_x = -K.pool3d(-x, pool_size=(1, 2, 3), strides=(1, 1, 1), padding='same')
+    return min_x
+
+
+def min_max_pool3d_output_shape(input_shape):
+    return input_shape
 
 
 def create_model(input_param, model_param, nb_steps, step_length, optimizer, type_loss=g.type_loss,
@@ -30,13 +40,23 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
     """
 
     # ---------- Model options ----------
-    mmodel_options ={
+    mmodel_options = {
         'dropout': g.dropout,
         'all_sequence': g.all_sequence,
-        'lstm_state': g.lstm_state
+        'lstm_state': g.lstm_state,
+        'no_batch_norm': False,
+        'bn_momentum': g.bn_momentum,
+        'lambdas_loss': g.lambdas_loss
     }
     mmodel_options.update(model_options)
 
+    dropout = mmodel_options['dropout']
+    all_sequence = mmodel_options['all_sequence']
+    lstm_state = mmodel_options['lstm_state']
+    batch_norm = not mmodel_options['no_batch_norm']
+    bn_momentum = mmodel_options['bn_momentum']
+
+    lambda_loss_activation, lambda_loss_duration = g.get_lambdas_loss(mmodel_options['lambdas_loss'])
     # --------- End model options ----------
 
     print('Definition of the graph ...')
@@ -55,66 +75,114 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
     for instrument in range(nb_instruments):
         inputs_midi.append(tf.keras.Input(midi_shape))  # [(batch, nb_steps, input_size, 2)]
 
-    # --------- Only activation ----------
-    inputs_activation = []
-    for instrument in range(nb_instruments):
-        inputs_activation.append(Lambda(lambda xl: xl[:, :, :, 0])(
-            inputs_midi[instrument]))  # activation (batch, nb_steps, step_length, input_size)
-        inputs_activation[instrument] = layers.Reshape((nb_steps, step_length, input_size, 1))(
-            inputs_activation[instrument])
-
     # ---------- All together ----------
-    x = layers.concatenate(inputs_midi, axis=4)  # (batch, nb_steps, step_length input_size, nb_instruments)
+    x = layers.concatenate(inputs_midi, axis=4)  # (batch, nb_steps, step_length, input_size, nb_instruments)
 
-    # ----- Fully connected layers -----
+    # ----- Convolutional Layer -----
+    convo = model_param['convo']
+    for i in convo:
+        for index, s in enumerate(i):
+            size = s
+            if index + 1 == len(i):
+                x = layers.Conv3D(filters=size, kernel_size=(1, 5, 5), strides=(1, 1, 2), padding='same')(x)
+            else:
+                x = layers.Conv3D(filters=size, kernel_size=(1, 5, 5), padding='same')(x)
+            if batch_norm:
+                x = layers.BatchNormalization(momentum=bn_momentum)(x)
+            x = layers.LeakyReLU()(x)
+            x = layers.Dropout(dropout / 2)(x)
+        # x = layers.MaxPool3D(pool_size=(1, 3, 3), strides=(1, 1, 2), padding='same')(x)
     shape_before_fc = x.shape
     x = layers.Reshape((x.shape[1], x.shape[2] * x.shape[3] * x.shape[4]))(
-        x)  # (batch, nb_steps, length * size * input_size)
-
-    x = layers.Dropout(0.5)(x)
-    # fc 1
-    x = layers.TimeDistributed(layers.Dense(64))(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.5)(x)
-    # fc 2
-    x = layers.TimeDistributed(layers.Dense(48))(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.4)(x)
-    # fc 2
-    x = layers.TimeDistributed(layers.Dense(32))(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.3)(x)
-
+        x)  # (batch, nb_steps, lenght * size * filters)
+    fc = model_param['fc']
+    for s in fc:
+        size = eval(s, env)
+        x = layers.TimeDistributed(layers.Dense(size))(x)
+        if batch_norm:
+            x = layers.BatchNormalization(momentum=bn_momentum)(x)
+        x = layers.LeakyReLU()(x)
+        x = layers.Dropout(dropout)(x)
     # ---------- LSTM -----------
-    x = layers.LSTM(64,
-                    return_sequences=False,
-                    return_state=False,
-                    unit_forget_bias=True,
-                    dropout=0.3,
-                    recurrent_dropout=0.3)(x)  # (batch, nb_steps, size)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.3)(x)
+    size_before_lstm = x.shape[2]  # (batch, nb_steps, size)
+    # -- Loop --
+    lstm = model_param['LSTM']
+    for s in lstm[:-1]:
+        size = eval(s, env)
+        x = layers.LSTM(size,
+                        return_sequences=True,
+                        unit_forget_bias=True,
+                        dropout=dropout,
+                        recurrent_dropout=dropout)(x)  # (batch, nb_steps, size)
+        if batch_norm:
+            x = layers.BatchNormalization(momentum=bn_momentum)(x)
+        x = layers.LeakyReLU()(x)
+        x = layers.Dropout(dropout)(x)
+    # -- Last one --
+    if lstm_state:
+        s = lstm[-1]
+        size = eval(s, env)
+        x, state_h, state_c = layers.LSTM(size,
+                                          return_sequences=all_sequence,
+                                          return_state=True,
+                                          unit_forget_bias=True,
+                                          dropout=dropout,
+                                          recurrent_dropout=dropout)(x)  # (batch, nb_steps, size)
+        if batch_norm:
+            x = layers.BatchNormalization(momentum=bn_momentum)(x)
+        x = layers.LeakyReLU()(x)
+        x = layers.Dropout(dropout)(x)
+        if all_sequence:
+            x = layers.Flatten()(x)
+
+        x = layers.concatenate([x, state_h, state_c], axis=1)  # (batch, 3 *  size)
+    else:
+        s = lstm[-1]
+        size = eval(s, env)
+        x = layers.LSTM(size,
+                        return_sequences=all_sequence,
+                        return_state=False,
+                        unit_forget_bias=True,
+                        dropout=dropout,
+                        recurrent_dropout=dropout)(x)  # (batch, nb_steps, size)
+        if batch_norm:
+            x =layers.BatchNormalization(momentum=bn_momentum)(x)
+        x = layers.LeakyReLU()(x)
+        if all_sequence:
+            x = layers.Flatten()(x)
+        x = layers.Dropout(dropout)(x)
+    x = layers.Dense(size_before_lstm)(x)  # (batch, size)
 
     # ----- Fully Connected ------
-    # fc 3
-    x = layers.Dense(32)(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.5)(x)
-    # fc 2
-    x = layers.Dense(48)(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.4)(x)
-    # fc 1
-    x = layers.Dense(64)(x)
-    x = layers.ReLU()(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dropout(0.3)(x)
+    fc_decoder = model_param['fc'][::-1]
+    for s in (fc_decoder[1:] + [shape_before_fc[2] * shape_before_fc[3] * shape_before_fc[4]]):
+        size = es.eval_all(s, env)
+        x = layers.Dense(size)(x)
+        if batch_norm:
+            x = layers.BatchNormalization(momentum=bn_momentum)(x)
+        x = layers.LeakyReLU()(x)
+        x = layers.Dropout(dropout)(x)  # (batch, size)
+
+    # ----- Transposed Convolution -----
+    x = layers.Reshape((1, shape_before_fc[2], shape_before_fc[3], shape_before_fc[4]))(
+        x)  # (batch, length, size, filters)
+    transposed_convo = model_param['convo'][::-1]
+    for i in range(len(transposed_convo)):  # inverse the lists inside
+        transposed_convo[i] = transposed_convo[i][::-1]
+    for i in range(len(transposed_convo) - 1):
+        transposed_convo[i] = transposed_convo[i][1:] + [transposed_convo[i + 1][0]]
+    transposed_convo[-1] = transposed_convo[-1][1:] + [model_param['last_convo']]
+    for i in transposed_convo:
+        for s in i:
+            size = s
+            x = layers.Conv3DTranspose(filters=size, kernel_size=(1, 5, 3), padding='same')(x)
+            if batch_norm:
+                x = layers.BatchNormalization(momentum=bn_momentum)(x)
+            x = layers.LeakyReLU()(x)
+            x = layers.Dropout(dropout / 2)(x)
+        x = layers.UpSampling3D(size=(1, 1, 2))(x)  # Batch size
+
+    x = layers.Flatten()(x)
 
     # ---------- Instruments separately ----------
     outputs = []  # (batch, nb_steps, nb_instruments, input_size)
@@ -131,14 +199,11 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
     model = tf.keras.Model(inputs=inputs_midi, outputs=outputs)
 
     # ------------------ Losses -----------------
-    lambda_activation = 20
-    lambda_duration = 0
-
     # Define losses dict
     losses = {}
     for i in range(nb_instruments):
-        losses['Output_{0}'.format(i)] = l.choose_loss(type_loss)(lambda_activation, lambda_duration)
+        losses['Output_{0}'.format(i)] = l.choose_loss(type_loss)(lambda_loss_activation, lambda_loss_duration)
 
     model.compile(loss=losses, optimizer=optimizer, metrics=[l.acc_act, l.mae_dur])
 
-    return model, losses, (lambda_activation, lambda_duration)
+    return model, losses, (lambda_loss_activation, lambda_loss_duration)
