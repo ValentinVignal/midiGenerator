a854c481eeeda05ef7a824db94afa74f4925b8af
Valentin <valentin.vignal@gmail.com>
2019-11-19 22:58:19 +0800

    Use metric

--- src/NN/losses.py
+++ src/NN/losses.py
@@ -245,10 +245,12 @@ def mae_dur(y_true, y_pred):
 
 
 def acc_mono(y_true, y_pred):
-    y_true_a = Lambda(lambda x: x[:, :, :, :, 0])(y_true)
-    y_pred_a = Lambda(lambda x: x[:, :, :, :, 0])(y_pred)
+    y_true_a = Lambda(lambda x: tf.gather(x, axis=4, indices=0))(y_true)
+    y_pred_a = Lambda(lambda x: tf.gather(x, axis=4, indices=0))(y_pred)
+    y_true_a_no_nan = tf.where(math.is_nan(y_true_a), tf.zeros_like(y_true_a), y_true_a)
+    y_pred_a_no_nan = tf.where(math.is_nan(y_true_a), tf.zeros_like(y_pred_a), y_pred_a)     # To apply loss only on non nan value in true Tensor
 
-    acc = tf.keras.metrics.categorical_accuracy(y_true_a, y_pred_a)
-
+    acc = tf.keras.metrics.categorical_accuracy(y_true_a_no_nan, y_pred_a_no_nan)
+    
     return tf.reduce_mean(acc, axis=None)
 
--- src/NN/models/RMVAE/nn_model.py
+++ src/NN/models/RMVAE/nn_model.py
@@ -148,8 +148,8 @@ def create_model(input_param, model_param, nb_steps, step_length, optimizer, typ
     # ------------------------------ Compile ------------------------------
 
     model.compile(loss=losses,
-                  optimizer=optimizer
-                  )  # , metrics=[l.acc_mono])
+                  optimizer=optimizer,
+                  metrics=[l.acc_mono])
     model.build([(None, *midi_shape) for inst in range(nb_instruments)])
 
     return model, losses, (lambda_loss_activation, lambda_loss_duration)
