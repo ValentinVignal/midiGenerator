5a8dd05caef03e7acd3bf1fe2889d4f4da9f3728
Valentin <valentin.vignal@gmail.com>
2019-12-17 00:11:29 +0800

    Remove RMVAE Rename PoE -> RMVAE

--- src/NN/Models/__init__.py
+++ src/NN/Models/__init__.py
@@ -1,31 +1,27 @@
-from .RMVAE import RMVAE as RMVAE
 from .AEMono import AEMono as AEMono
 from .AEMonoRep import AEMonoRep
-from .PoEMono import PoEMono
-from .PoEMonoRep import PoEMonoRep
+from .RMVAEMono import RMVAEMono
+from .RMVAEMonoRep import RMVAEMonoRep
 from src.NN import Sequences
 
 from_name = dict(
-    RMVAE=RMVAE,
     AEMono=AEMono,
     AEMonoRep=AEMonoRep,
-    PoEMono=PoEMono,
-    PoEMonoRep=PoEMonoRep
+    RMVAEMono=RMVAEMono,
+    RMVAEMonoRep=RMVAEMonoRep
 )
 
 sequences = dict(
-    RMVAE=Sequences.MissingInstSequence.predict,
     AEMono=Sequences.AllInstSequence.predict,
     AEMonoRep=Sequences.AllInstSequence.replicate,
-    PoEMono=Sequences.MissingInstSequence.predict,
-    PoEMonoRep=Sequences.MissingInstSequence.replicate
+    RMVAEMono=Sequences.MissingInstSequence.predict,
+    RMVAEMonoRep=Sequences.MissingInstSequence.replicate
 )
 
 needs_mask = dict(
-    RMVAE=True,
     AEMono=False,
     AEMonoRep=False,
-    PoEMono=True,
-    PoEMonoRep=True
+    RMVAEMono=True,
+    RMVAEMonoRep=True
 )
 
