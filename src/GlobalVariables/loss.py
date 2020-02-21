4d7e7872bc0a1d99c2ff3e7195884cd2aed6ef6e
Valentin <valentin.vignal@gmail.com>
2020-02-21 16:06:29 +0800

    Change default values

--- src/GlobalVariables/loss.py
+++ src/GlobalVariables/loss.py
@@ -9,10 +9,10 @@ def get_lambdas_loss(lambdas_loss):
 
 
 lambda_loss_activation, lambda_loss_duration = get_lambdas_loss(lambdas_loss)
-l_scale = 1
-l_rhythm = 1
-l_scale_cost = 1e-2
-l_rhythm_cost = 1e-2
+l_scale = 1e-0
+l_rhythm = 1e-1
+l_scale_cost = 1e-3
+l_rhythm_cost = 1e-5
 take_all_step_rhythm = False
 
 
--- src/GlobalVariables/nn.py
+++ src/GlobalVariables/nn.py
@@ -1,9 +1,9 @@
 # ----- Optimizer -----
-lr = 5e-2
+lr = 1e-3
 # dropout = 1e-2
-dropout_d = 1e-1        # For dense layers
-dropout_c = 1e-1        # For convolutional layers
-dropout_r = 1e-1        # For RNN layers
+dropout_d = 2e-1        # For dense layers
+dropout_c = 1e-2        # For convolutional layers
+dropout_r = 2e-1        # For RNN layers
 decay = 1e-1
 epochs_drop = 50
 decay_drop = 0.1
