00f5ecf26447286bc2c992b38bd18c2171e2c583
Valentin <valentin.vignal@gmail.com>
2020-04-06 17:09:10 +0800

    param 2 function to have input of tensorboard command line

--- src/res/tb.py
+++ src/res/tb.py
@@ -3,8 +3,12 @@ File to compute the graph of the results from the train on the server
 """
 import os
 import matplotlib.pyplot as plt
+import argparse
+import sys
 
 from epicpath import EPath
+
+sys.path.append(os.path.abspath('.'))
 
 from src import tb as tb
 
@@ -38,30 +42,34 @@ def get_all_data(paths):
     return data
 
 
-def plot_several_tensorboard(data, label, run_labels=None, max_length=None, train=True, validation=False):
+def plot_several_tensorboard(data, label, run_names=None, run_labels=None, max_length=None, train=True,
+                             validation=False):
     """
 
+    :param run_labels:
     :param validation:
     :param train:
     :param max_length:
-    :param run_labels:
+    :param run_names:
     :param data:
     :param label:
     :return:
     """
-    run_labels = list(data.keys()) if run_labels is None else run_labels
+    run_names = list(data.keys()) if run_names is None else run_names
+    if run_labels is None:
+        run_labels = run_names
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
     plt.figure()
     plt.title(label)
     validation_linestyle = '--' if train else '-'
-    for i, run_label in enumerate(run_labels):
-        train, validation = data[run_label]['train'][label], data[run_label]['validation'][label]
+    for i, run_name in enumerate(run_names):
+        train, validation = data[run_name]['train'][label], data[run_name]['validation'][label]
         if train:
-            plt.plot(train[0][:max_length], train[1][:max_length], label=f'Train {run_label}', linestyle='-',
+            plt.plot(train[0][:max_length], train[1][:max_length], label=f'Train {run_labels[i]}', linestyle='-',
                      color=colors[i])
         if validation:
-            plt.plot(validation[0][:max_length], validation[1][:max_length], label=f'Val {run_label}',
+            plt.plot(validation[0][:max_length], validation[1][:max_length], label=f'Val {run_labels[i]}',
                      linestyle=validation_linestyle, color=colors[i])
     plt.xlabel('Epochs')
     plt.ylabel('Value')
@@ -80,5 +88,38 @@ def plot_from_names(names, label, *args, **kwargs):
     paths = [name_to_foler(name) for name in names]
     data = get_all_data(paths)
     plot_several_tensorboard(*args, data=data, label=label, **kwargs)
+
+
+def get_input_tensorboard_command(names=None):
+    """
+
+    :param names: list of strings
+    :return:
+    """
+    if names is None:
+        names = os.listdir(EPath(*['..' for _ in range(3)], 'big_runs').str)
+    root = EPath(*['..' for _ in range(3)], 'big_runs')
+    inputs = [
+        f'{name}:/{(root / name / "saved_models" / os.listdir(root / name / "saved_models")[0] / "tensorboard").as_posix()}'
+        for name in names
+    ]
+    return ','.join(inputs)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Get the logdir for tensorboard',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    print(os.listdir('.'))
+    parser.add_argument('-n', '--names', default=None, help='The considered names')
+    args = parser.parse_args()
+    if args.names is not None:
+        args.names = args.names.split(',')
+    res = get_input_tensorboard_command(args.names)
+    print(res)
+
+
+
 
 
